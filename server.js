6417144fbd58c8230b34239725420dd827321b9e
ALi <ALi@gmail.com>
2024-09-23 18:00:55 +0500

    Initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Arcana Info Project!');
+    res.send('Arcana info Project!');
 });
 
 app.listen(PORT, () => {
